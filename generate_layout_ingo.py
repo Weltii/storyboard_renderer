43d40d8a9bc982ff64789f18fcd17e81e716fdf9
Benjamin Krause <benjamin.krause@outlawraspberry.de>
2020-06-29 13:12:55 +0200

    Add movie layout generator

---
+++ generate_layout_ingo.py
@@ -0,0 +1,26 @@
+json = dict(
+	title="",
+	author="",
+	frames=[
+		dict(
+			scene="001",
+			setting="002",
+			section="001",
+			timecode="0:00:00:01",
+			image="./example_image.png",
+			image_description="asdnasjkdlf h",
+			audio_description="asdfa fdhajkdf hajdfkl",
+			setting_size="Halb Nah",
+			perspective="Vogel",
+			focal_length="85",
+			camera_movement="Statisch",
+			camera_support="Stativ",
+			fx="-"
+		)
+	]
+)
+
+
+def generate_frame():
+	string: str = "help"
+	pass
