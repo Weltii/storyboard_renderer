318fcad91d4a90956c19383facdd35bdb9e3a0b7
Benjamin Krause <benjamin.krause@outlawraspberry.de>
2020-06-30 20:13:25 +0200

    Add first web interface prototype

--- main.py
+++ main.py
@@ -1,37 +1,45 @@
 import uvicorn
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
 from layout_bridge import compile_layout
+from fastapi.responses import RedirectResponse
+from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 import json
 
 
 app = FastAPI()
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
 
-def generate_html_response():
-    html_content = """
-    <html>
-        <head>
-            <title>Some HTML in here</title>
-        </head>
-        <body>
-            <h1>Look ma! HTML!</h1>
-        </body>
-    </html>
-    """
-    return HTMLResponse(content=html_content, status_code=200)
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
-    return generate_html_response()
+    return RedirectResponse("/web/dist/index.html")
 
 
 @app.get("/storyboard/")
 def render_json(payload: str):
     j = json.loads(payload)
     path = compile_layout("movie", j)
-    return {"pdf_path": path}
+    return {"path": path}
+
+
+app.mount("/web", StaticFiles(directory="web"), name="web")
+
+app.mount("/output", StaticFiles(directory="output"), name="pdf-output")
 
 
 if __name__ == "__main__":
