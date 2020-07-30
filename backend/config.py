1a5eb3fa6305e372856f8cad21d2c23a4ce2a96c
Benjamin Krause <benjamin.krause@outlawraspberry.de>
2020-07-30 15:21:49 +0200

    Expand config and add sample data

--- backend/config.py
+++ backend/config.py
@@ -1,3 +1,14 @@
 import os
 
-output_path = os.path.join(os.path.dirname(__file__), "output")
+# paths
+
+base_path = os.path.dirname(__file__)
+output_path = os.path.join(base_path, "output")
+sample_data_path = os.path.join(base_path, "sample_data")
+
+# latex settings
+
+# latex path or terminal command
+# I recommend luatex or pdftex
+# If you wanna use lua code inside the templates, you need luatex!
+latex_compiler = "luatex"
