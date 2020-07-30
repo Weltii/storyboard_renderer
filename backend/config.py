5d72ba71e37818eeedf825e6ed67f247de053b67
Benjamin Krause <benjamin.krause@outlawraspberry.de>
2020-07-30 16:20:12 +0200

    Fix latex_compiler

--- backend/config.py
+++ backend/config.py
@@ -9,6 +9,6 @@ sample_data_path = os.path.join(base_path, "sample_data")
 # latex settings
 
 # latex path or terminal command
-# I recommend luatex or pdftex
-# If you wanna use lua code inside the templates, you need luatex!
-latex_compiler = "luatex"
+# I recommend lualatex or pdftex
+# If you wanna use lua code inside the templates, you need lualatex!
+latex_compiler = "lualatex"
