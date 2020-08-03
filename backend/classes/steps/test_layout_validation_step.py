bdce799cc693464e097ff936ba5a5d089183351d
Benjamin Krause <benjamin.krause@outlawraspberry.de>
2020-08-03 16:42:34 +0200

    Adjust layout validation step test

--- backend/classes/steps/test_layout_validation_step.py
+++ backend/classes/steps/test_layout_validation_step.py
@@ -3,6 +3,7 @@ import unittest
 from backend.classes.render_job import Job
 from backend.classes.steps.layout_validation_step import LayoutValidationStep
 from backend.classes.storyboard import Storyboard
+from backend.sample_data.sample_generator import generate_sample_project
 from backend.utils.enums import LayoutName
 
 storyboard = Storyboard(
@@ -12,11 +13,11 @@ storyboard = Storyboard(
 
 class TestLayoutValidationStep(unittest.TestCase):
     def test_run_with_valid_layout(self):
-        job = Job(layout=LayoutName.EASY_LAYOUT.value, storyboard=storyboard)
+        job = Job(layout=LayoutName.EASY_LAYOUT.value, project=generate_sample_project())
         self.assertTrue(LayoutValidationStep.run(job))
 
     def test_run_with_invalid_layout(self):
-        job = Job(layout="SuperHeroLayout", storyboard=storyboard)
+        job = Job(layout="SuperHeroLayout", project=generate_sample_project())
         self.assertFalse(LayoutValidationStep.run(job))
 
 
